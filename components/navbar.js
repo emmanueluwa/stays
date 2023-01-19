35c5b1fdbb45aec1974f3292285bf89098377b18
emmanueluwa <emmanuel@fulodev.com>
2023-01-19 16:31:56 +0000

    initial commit with navbar menu icon

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+import { CgMenuGridO } from "react-icons/cg";
+
+export default function Navbar({}) {
+  return (
+    <div>
+      {/* left menu */}
+      <div className="">
+        <CgMenuGridO className="w-10 h-10" />
+      </div>
+      {/* middle logo*/}
+      {/* right authentication */}
+    </div>
+  );
+}

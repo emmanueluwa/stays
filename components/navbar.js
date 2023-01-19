52ddf8c55b37ce4d19bfebb34bfa3d0c9386b070
emmanueluwa <emmanuel@fulodev.com>
2023-01-19 21:19:50 +0000

    adding to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,14 +1,30 @@
 import { CgMenuGridO } from "react-icons/cg";
+import { HiOutlineUser } from "react-icons/hi";
+import Image from "next/image";
 
 export default function Navbar({}) {
   return (
-    <div>
+    <div className="flex items-center ">
       {/* left menu */}
       <div className="">
         <CgMenuGridO className="w-10 h-10" />
       </div>
       {/* middle logo*/}
+      <div>
+        <Image
+          priority
+          src="/images/a_house.png"
+          width={80}
+          height={80}
+          alt=""
+        />
+      </div>
       {/* right authentication */}
+      <div className="">
+        <div className="w-10 h-10 bg-[black] rounded-full">
+          <HiOutlineUser />
+        </div>
+      </div>
     </div>
   );
 }

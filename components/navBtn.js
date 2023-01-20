152b4a1d4032bdd260d5d37752ed7d7a66e15f2d
emmanueluwa <emmanuel@fulodev.com>
2023-01-20 17:41:06 +0000

    adding finished navbar

--- components/navBtn.js
+++ components/navBtn.js
@@ -1,9 +1,13 @@
-export default function NavBtn({ title }) {
+export default function NavBtn({ title, border, underline }) {
   return (
     <div>
-      <li className="relative group mx-2 font-semibold">
-        <button className="">{title}</button>
-        <div className="absolute group-hover:flex hidden h-1 w-full bg-[#d8a90f] -bottom-[1.2]"></div>
+      <li className="relative group mx-2 font-semibold whitespace-nowrap">
+        <button className={`px-2 ${border}`}>{title}</button>
+        <div
+          className={`absolute group-hover:flex hidden h-1 w-full bg-[#d8a90f] ${
+            underline ? underline : "-bottom-[0.7rem]"
+          }`}
+        ></div>
       </li>
     </div>
   );
--- components/navbar.js
+++ components/navbar.js
@@ -5,53 +5,62 @@ import NavBtn from "./navBtn";
 
 export default function Navbar({}) {
   const buttons = [
-    { title: "Rent" },
-    { title: "Short stays" },
-    { title: "Blog" },
-    { title: "About us" },
-    { title: "Advertise", border: "border" },
+    { title: "Rent", underline: "-bottom-[1.2]" },
+    { title: "Short stays", underline: "-bottom-[1.2]" },
+    { title: "Blog", underline: "-bottom-[1.2]" },
+    { title: "About us", underline: "-bottom-[1.2]" },
+    {
+      title: "Advertise",
+      border: "border p-1 rounded-[0.5rem] border-grey-900",
+    },
   ];
   return (
-    <div className="flex justify-between bg-white px-4 items-center">
-      {/* left menu */}
-      <div className="lg:hidden flex items-center">
-        <CgMenuGridO className="w-10 h-10" />
-      </div>
-      {/* middle logo*/}
-      <div className="">
-        {/* Logo */}
-        <div className="">
-          <Image
-            priority
-            src="/images/a_house.png"
-            width={80}
-            height={80}
-            alt=""
-            className=""
-          />
+    <div className="bg-white">
+      <div className="flex justify-between  px-4 items-center max-w-[95vw] sm:max-w-[85vw] lg:max-w-[85vw] mx-auto whitespace-nowrap">
+        {/* left menu */}
+        <div className="lg:hidden flex items-center">
+          <CgMenuGridO className="w-10 h-10" />
         </div>
-      </div>
-
-      {/* links */}
-      <div className="hidden lg:flex">
-        <ul className="flex space-x-10   py-4">
-          {buttons.map((button) => (
-            <NavBtn title={button.title} />
-          ))}
-        </ul>
-      </div>
-
-      {/* right authentication */}
-      <div className="lg:hidden flex h-full items-center">
-        <div className="w-10 h-10 bg-[white] rounded-full flex justify-center ">
-          <HiOutlineUser className="text-[38px]" />
+        {/* middle logo*/}
+        <div className="shrink-0 pr-6">
+          {/* Logo */}
+          <div className="">
+            <Image
+              priority
+              src="/images/a_house.png"
+              width={80}
+              height={80}
+              alt=""
+              className="shrink-0"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="hidden lg:flex items-center">
-        <button>Log in</button>
-        <div className="bg-[#d8a90f] p-2 rounded-[0.5rem] px-4 ml-4 ">
-          <button>Sign up</button>
+        {/* links */}
+        <div className="hidden lg:flex">
+          <ul className="flex space-x-10 items-center  py-4">
+            {buttons.map((button) => (
+              <NavBtn
+                title={button.title}
+                border={button.border}
+                underline={button.underline}
+              />
+            ))}
+          </ul>
+        </div>
+
+        {/* right authentication */}
+        <div className="lg:hidden flex h-full items-center">
+          <div className="w-10 h-10 bg-[white] rounded-full flex justify-center ">
+            <HiOutlineUser className="text-[38px]" />
+          </div>
+        </div>
+
+        <div className="hidden lg:flex items-center pl-8">
+          <button>Log in</button>
+          <div className="bg-[#d8a90f] p-2 rounded-[0.5rem] px-4 ml-4 flex items-center">
+            <button>Sign up</button>
+          </div>
         </div>
       </div>
     </div>

94c4fc3cb556cdfdfadcff359f7bea196ec45d98
emmanueluwa <emmanuel@fulodev.com>
2023-01-31 23:07:37 +0000

    adding links and pages for register and login

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar";
+
+
+interface Props {
+  
+}
+
+export default function Login() {
+    return (
+      <Navbar>
+        <div>login</div>
+      </Navbar>
+    )
+}
---
+++ pages/register.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar";
+
+interface Props {
+
+}
+
+export default function Register() {
+    return(
+      <Navbar>
+        <div>register</div>
+      </Navbar>
+    ) 
+}

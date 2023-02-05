ae0f857e8026cc622a5faa059bac22851dc3613a
emmanueluwa <emmanuel@fulodev.com>
2023-02-05 09:57:50 +0000

    adding styling to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -20,62 +20,82 @@ export default function Login() {
 
     return(
       <Navbar>
-        <div>login</div>
-        <form onSubmit={async e => {
-          e.preventDefault()
-          console.log('form submitted');
-          const response = await login({
-            variables: {
-              email,
-              password
-            },
-            //updating apollo cache
-            update: (store, {data}) => {
-              if (!data) {
-                return null;
-              }
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="w-full max-w-md space-y-8">
+            <div className="">
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">login</h2>
+            </div>
+            <form className="mt-8 space-y-6"
+                onSubmit={async e => {
+                  e.preventDefault()
+                  console.log('form submitted');
+                  const response = await login({
+                    variables: {
+                      email,
+                      password
+                    },
+                    //updating apollo cache
+                    update: (store, {data}) => {
+                      if (!data) {
+                        return null;
+                      }
 
-              //details when logging in user must match details when fetching 'me' #apollocache
-              store.writeQuery<MeQuery>({
-                query: MeDocument,
-                data: {
-                  __typename: "Query",
-                  me: data.login.user
+                      //details when logging in user must match details when fetching 'me' #apollocache
+                      store.writeQuery<MeQuery>({
+                        query: MeDocument,
+                        data: {
+                          __typename: "Query",
+                          me: data.login.user
+                        }
+                      })
+                      
+                    }
+                  });
+
+                console.log(response);
+
+                if (response && response.data) {
+                  setAccessToken(response.data.login.accessToken)
                 }
-              })
-              
-            }
-          });
 
-          console.log(response);
+                router.push("/");
+              }}>
+              <div className="-space-y-px rounded-md shadow-sm">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">Email</label>
+                  <input
+                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-[#d8a90f] focus:outline-none focus:ring-[#d8a90f] sm:text-sm"
+                    value={email}
+                    placeholder="email"
+                    onChange={e => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                </div>
 
-          if (response && response.data) {
-            setAccessToken(response.data.login.accessToken)
-          }
-
-          router.push("/");
-        }}>
-          <div>
-            <input
-              value={email}
-              placeholder="email"
-              onChange={e => {
-                setEmail(e.target.value);
-              }}
-            />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">Password</label>
+                <input
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-[#d8a90f] focus:outline-none focus:ring-[#d8a90f] sm:text-sm"
+                  type="password"
+                  value={password}
+                  placeholder="password"
+                  onChange={e => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </div>
+              <button 
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#d8a90f] py-2 px-4 text-sm font-medium text-black hover:bg-[#d8a90f] focus:outline-none focus:ring-2 focus:ring-[#d8a90f] focus:ring-offset-2"
+                type="submit"
+                >
+                  login
+                </button>
+            </form>
           </div>
-          <div>
-            <input
-              type="password"
-              value={password}
-              placeholder="password"
-              onChange={e => {
-                setPassword(e.target.value);
-              }}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
+        </div>
+
       </Navbar>
     ) 
 }

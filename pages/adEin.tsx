2113289c5eb52cc4d702163aedaebddbabd3bc0f
emmanueluwa <emmanuel@fulodev.com>
2023-02-15 22:32:15 +0000

    adding part 2/3 for ad service

--- pages/adEin.tsx
+++ pages/adEin.tsx
@@ -4,70 +4,71 @@ import router from "next/router";
 import { Fragment, useState } from "react";
 import Navbar from "../components/navbar";
 
-const bedrooms = [
-    {
-        id: 1,
-        beds: "1 bed"
-    },
-    {
-        id: 2,
-        beds: "2 bed"
-    },
-    {
-        id: 3,
-        beds: "3 bed"
-    },
-    {
-        id: 4,
-        beds: "4 bed"
-    },
-    {
-        id: 5,
-        beds: "5 bed"
-    },
-    {
-        id: 6,
-        beds: "6 bed"
-    },
-    {
-        id: 7,
-        beds: "7 bed"
-    },
-    {
-        id: 8,
-        beds: "8 bed"
-    },
-    {
-        id: 9,
-        beds: "9 bed"
-    },
-    {
-        id: 10,
-        beds: "10 bed"
-    },
-    {
-        id: 11,
-        beds: "11 bed"
-    },
-    {
-        id: 12,
-        beds: "12 bed"
-    },
-]
-const propertyType = [
-    {
-        id: 1,
-        property: "Flat/Apartment"
-    },
-    {
-        id: 2,
-        property: "House"
-    },
-    {
-        id: 3,
-        property: "Property"
-    }
-]
+// data for 2nd type of dropdown list
+// const bedrooms = [
+//     {
+//         id: 1,
+//         beds: "1 bed"
+//     },
+//     {
+//         id: 2,
+//         beds: "2 bed"
+//     },
+//     {
+//         id: 3,
+//         beds: "3 bed"
+//     },
+//     {
+//         id: 4,
+//         beds: "4 bed"
+//     },
+//     {
+//         id: 5,
+//         beds: "5 bed"
+//     },
+//     {
+//         id: 6,
+//         beds: "6 bed"
+//     },
+//     {
+//         id: 7,
+//         beds: "7 bed"
+//     },
+//     {
+//         id: 8,
+//         beds: "8 bed"
+//     },
+//     {
+//         id: 9,
+//         beds: "9 bed"
+//     },
+//     {
+//         id: 10,
+//         beds: "10 bed"
+//     },
+//     {
+//         id: 11,
+//         beds: "11 bed"
+//     },
+//     {
+//         id: 12,
+//         beds: "12 bed"
+//     },
+// ]
+// const propertyType = [
+//     {
+//         id: 1,
+//         property: "Flat/Apartment"
+//     },
+//     {
+//         id: 2,
+//         property: "House"
+//     },
+//     {
+//         id: 3,
+//         property: "Property"
+//     }
+// ]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -75,19 +76,21 @@ function classNames(...classes) {
 
 export default function AddEin() {
 
-    const [selected, setSelected] = useState(bedrooms[0])
-    const [typeSelected, setTypeSelected] = useState(propertyType[0])
+    // data for 2nd type of dropdown list
+    // const [selected, setSelected] = useState(bedrooms[0])
+    // const [typeSelected, setTypeSelected] = useState(propertyType[0])
 
 
     return (
         <Navbar>
          <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="flex flex-row flex-wrap py-4 space-y-8">
+            
             <main className="sm:w-2/3 md:w-3/4 pt-1 px-2 space-y-8 text-left">
 
               {/* title */}
                 <div className="mx-8">
-                  <h1 className="text-[24px]  font-medium">Post an Advert Setp 1 of 3</h1>
+                  <h1 className="text-[24px]  font-medium">Post an Advert Step 1 of 3</h1>
                 </div>
 
               {/* form  */}
@@ -96,74 +99,51 @@ export default function AddEin() {
                 onSubmit={async e => {
                     // prevent browser from reloading page
                     e.preventDefault()
-                    router.push("/adZwei");
+                    router.push({
+                        pathname: "/adZwei",
+                    });
                   }}
               >
                 <div>
-                    <Listbox value={selected} onChange={setSelected}>
-                        {({ open }) => (
-                            <>
-                            <Listbox.Label className="block text-sm font-medium text-gray-700">I have a</Listbox.Label>
-                            <div className="relative mt-1">
-                                <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:[#d8a90f] focus:outline-none focus:ring-1 focus:ring-[#d8a90f] sm:text-sm">
-                                <span className="flex items-center">
-                                    <span className="ml-3 block truncate">{selected.beds}</span>
-                                </span>
-                                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                                    <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                </span>
-                                </Listbox.Button>
+                    <label htmlFor="bedrooms" className="block text-sm font-medium text-gray-700">
+                        Beds
+                      </label>
+                      <select
+                        id="bedrooms"
+                        name="beds"
+                        autoComplete="1 bed"
+                        className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-[#d8a90f] focus:outline-none focus:ring-[#d8a90f] sm:text-sm"
+                      >
+                        <option>1 bed</option>
+                        <option>2 bed</option>
+                        <option>3 bed</option>
+                        <option>4 bed</option>
+                        <option>5 bed</option>
+                        <option>6 bed</option>
+                        <option>7 bed</option>
+                        <option>8 bed</option>
+                        <option>9 bed</option>
+                        <option>10 bed</option>
+                        <option>11 bed</option>
+                        <option>12 bed</option>
+                      </select>
+                    
+                     <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                          Property type
+                        </label>
+                        <select
+                          id="country"
+                          name="country"
+                          autoComplete="country-name"
+                          className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-[#d8a90f] focus:outline-none focus:ring-[#d8a90f] sm:text-sm"
+                        >
+                          <option>Flat/Apartment</option>
+                          <option>House</option>
+                          <option>Property</option>
+                        </select>
 
-                                <Transition
-                                show={open}
-                                as={Fragment}
-                                leave="transition ease-in duration-100"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                                >
-                                <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                    {bedrooms.map((bed) => (
-                                    <Listbox.Option
-                                        key={bed.id}
-                                        className={({ active }) =>
-                                        classNames(
-                                            active ? 'text-white bg-[#d8a90f]' : 'text-gray-900',
-                                            'relative cursor-default select-none py-2 pl-3 pr-9'
-                                        )
-                                        }
-                                        value={bed}
-                                    >
-                                        {({ selected, active }) => (
-                                        <>
-                                            <div className="flex items-center">
-                                            <span
-                                                className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                                            >
-                                                {bed.beds}
-                                            </span>
-                                            </div>
-
-                                            {selected ? (
-                                            <span
-                                                className={classNames(
-                                                active ? 'text-white' : 'text-[#d8a90f]',
-                                                'absolute inset-y-0 right-0 flex items-center pr-4'
-                                                )}
-                                            >
-                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                            </span>
-                                            ) : null}
-                                        </>
-                                        )}
-                                    </Listbox.Option>
-                                    ))}
-                                </Listbox.Options>
-                                </Transition>
-                            </div>
-                            </>
-                        )}
-                    </Listbox>
-                    <Listbox value={typeSelected} onChange={setTypeSelected}>
+                    {/* 2nd type of dropdown list option  */}
+                    {/* <Listbox value={typeSelected} onChange={setTypeSelected}>
                         {({ open }) => (
                             <>
                             <Listbox.Label className="block text-sm font-medium text-gray-700"></Listbox.Label>
@@ -225,7 +205,7 @@ export default function AddEin() {
                             </div>
                             </>
                         )}
-                    </Listbox>
+                    </Listbox> */}
                 </div>
                 
                 <div className="flex space-x-2 justify-center">
--- pages/adZwei.tsx
+++ pages/adZwei.tsx
@@ -1,9 +1,180 @@
 import Navbar from "../components/navbar";
+import router from "next/router";import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid"
+import { Fragment, useState } from "react";
+
+const rents = [
+    {
+        id: 1,
+        rentType: "Per week"
+    },
+    {
+        id: 2,
+        rentType: "Per month"
+    },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function AdZwei() {
+
+    const [selected, setSelected] = useState(rents[0])
+
     return (
-      <Navbar>
-        <div>AdZwei</div>
-      </Navbar>
+       <Navbar>
+         <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-row flex-wrap py-4 space-y-8">
+            
+            <div className="mx-8">
+                  <h1 className="text-[24px]  font-medium">Post an Advert Step 2 of 3</h1>
+                </div>
+            <main className="sm:w-2/3 md:w-3/4 pt-1 px-2 space-y-8 text-left">
+
+              {/* title */}
+                <div className="mx-8">
+                  <h1 className="text-[24px]  font-medium">We need a few basic details first</h1>
+                </div>
+
+              {/* form  */}
+              <form 
+                className="space-y-8"
+                onSubmit={async e => {
+                    // prevent browser from reloading page
+                    e.preventDefault()
+                    router.push({
+                        pathname: "/adZwei",
+                    });
+                  }}
+              >
+                <div>
+                    <label htmlFor="" className="block text-sm font-medium text-gray-700 py-4">Postcode of property</label>
+                    <div>
+                      <input
+                        className="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-[#d8a90f] focus:outline-none focus:ring-[#d8a90f] sm:text-sm"
+                        type=""
+                        value=""
+                        placeholder=""
+                      />
+                      <p className="mt-1 text-sm text-gray-500">
+                      (e.g LS1 4AD)
+                      </p>
+                    </div>
+                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700 py-4">
+                          Rent (for the whole property)
+                    </label>
+                    <div className="flex">
+                      <div className="col-span-6 sm:col-span-3">
+                        
+                        <input
+                          type="text"
+                          name="rent"
+                          id="rent"
+                          autoComplete="rent"
+                          className="relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-[#d8a90f] focus:outline-none focus:ring-[#d8a90f] sm:text-sm"
+                        />
+                      </div>
+                      <div className="col-span-6 sm:col-span-3">
+                        <label htmlFor="payments" className="text-sm font-medium text-gray-700 hidden">
+                          Payments
+                        </label>
+                        <select
+                          id="payments"
+                          name="counpayments"
+                          autoComplete="payments-type"
+                          className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-[#d8a90f] focus:outline-none focus:ring-[#d8a90f] sm:text-sm"
+                        >
+                          <option>Per week</option>
+                          <option>Per month</option>
+                        </select>
+                      </div>
+                      
+                    </div>
+
+                    <div className="mt-5 md:col-span-2 md:mt-0 pt-4">
+                      <div className="overflow-hidden shadow-2xl sm:rounded-md">
+                        <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                          <fieldset>
+                            <legend className="sr-only">I am a</legend>
+                            <div className="text-base font-medium text-gray-900" aria-hidden="true">
+                              I am a
+                            </div>
+                            <div className="mt-4 space-y-4">
+                              <div className="flex items-start">
+                                <div className="flex h-5 items-center">
+                                  <input
+                                    id="comments"
+                                    name="comments"
+                                    type="checkbox"
+                                    className="h-4 w-4 rounded border-gray-300 text-[#d8a90f] focus:ring-[#d8a90f]"
+                                  />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                  <label htmlFor="comments" className="font-medium text-gray-700">
+                                    live out landlord
+                                  </label>
+                                  <p className="text-gray-500">(I own the property but don't live there)</p>
+                                </div>
+                              </div>
+                              <div className="flex items-start">
+                                <div className="flex h-5 items-center">
+                                  <input
+                                    id="candidates"
+                                    name="candidates"
+                                    type="checkbox"
+                                    className="h-4 w-4 rounded border-gray-300 text-[#d8a90f] focus:ring-[#d8a90f]"
+                                  />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                  <label htmlFor="candidates" className="font-medium text-gray-700">
+                                    agent
+                                  </label>
+                                  <p className="text-gray-500">(I am a advertising on a landlord's behalf)</p>
+                                </div>
+                              </div>
+                              <div className="flex items-start">
+                                <div className="flex h-5 items-center">
+                                  <input
+                                    id="offers"
+                                    name="offers"
+                                    type="checkbox"
+                                    className="h-4 w-4 rounded border-gray-300 text-[#d8a90f] focus:ring-[#d8a90f]"
+                                  />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                  <label htmlFor="offers" className="font-medium text-gray-700">
+                                    current tenant(s)
+                                  </label>
+                                  <p className="text-gray-500">(I want to move out but need someone to replace me)</p>
+                                </div>
+                              </div>
+                            </div>
+                          </fieldset>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div className="flex space-x-2 justify-center">
+                    <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-[#d8a90f] py-2 px-4 text-sm font-medium text-black hover:bg-[#d8a90f] focus:outline-none focus:ring-2 focus:ring-[#d8a90f] focus:ring-offset-2">
+                    Continue &gt;&gt;
+                    </button>
+                </div>
+                {/* accepted payments */}
+                {/* <h1>We will be accepting the following payments methods</h1> */}
+            </form>
+            </main>
+            <aside className="w-full sm:w-1/3 md:w-1/4 px-2 items-end">
+              <div className="sticky top-0 p-4 w-full">
+                <div className="flex items-center py-4">
+                  <div className="w-2 h-2 bg-[#d8a90f] rounded mr-1"></div>
+                  <p>Need help?</p>
+                </div>
+                <p>Contact us by <a>email</a></p>
+              </div>
+            </aside>
+          </div>
+        </div>
+        </Navbar>
     )
 }
